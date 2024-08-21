43ea7e29359e8216ca31f556eb54225a376e5e12
Isabel Urrutia <profesora.isabel.u.a@gmail.com>
2024-08-20 21:36:52 -0400

    Home y CardPizza sin esilo, antes de quitar bootstrap

--- src/components/CardPizza.jsx
+++ src/components/CardPizza.jsx
@@ -1,18 +1,22 @@
 import { separador_de_miles } from "../utils/funciones_valores"
 import '../style/CardPizza.css'
 
-
-const CardPizza = ({name, price, ingredients, img}) => {
-    let ingr = ingredients.join(', ')
+const CardPizza = ({name, price, ingredients, img, id, desc}) => {
     return (
-        <div className="Pizzas">
+        <div className="Pizzas" key={id}>
             <img src={img} alt={name} />
             <div className="NombrePizza">
-                <h3>Pizza {name}</h3>
+                <h3>Pizza {name} {id}</h3>
+                <p>{desc}</p>
             </div>
             <div className="Ingredientes">
                 <p>Ingredientes</p>
-                <p>🍕 {ingr}</p>
+                <ul>
+                    {ingredients.map((ingredient, index) => (
+                        <li key={index}>🍕 {ingredient}</li>
+                    ))}
+                </ul>
+                
             </div>
             <h2>Precio: ${separador_de_miles(price)}</h2>
             <div className="botonesCard">
