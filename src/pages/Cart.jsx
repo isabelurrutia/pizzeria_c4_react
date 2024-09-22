ffbd939043ebf60382d308d73c8cedb0109d56ae
Isabel Urrutia <profesora.isabel.u.a@gmail.com>
2024-09-22 19:01:18 -0300

    hito 7: punto 1,2,3 y 4 terminados

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -4,10 +4,12 @@ import {useContext, useState} from 'react'
 import { separador_de_miles } from "../utils/funciones_valores"
 import '../style/Cart.css'
 import MyContext from '../context/MyContext'
+import ContextToken from '../context/ContextToken'
 
 const Cart = () => {
 
     const {listaProductos, setListaProductos, totalCompra, setTotalCompra}= useContext(MyContext)
+    const { token } = useContext(ContextToken);
 
     // Función para eliminar un producto del carrito
     const eliminarProducto = (name) => {
@@ -37,7 +39,12 @@ const Cart = () => {
             </div>
             <div className='totalGeneral'>
                 <h2>Total General de la Compra: ${separador_de_miles(totalCompra)}</h2>
-                <button className='pagarCarro'><strong>Pagar</strong></button>
+                <button 
+                    className={token ? 'pagarCarro' : 'pagarCarroDisabled'} 
+                    disabled={!token}  // Deshabilitar el botón si el token es false
+                >
+                    <strong>Pagar</strong>
+                </button>
             </div>
         </>
     )
