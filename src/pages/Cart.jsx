8d6cd50641fe1b1df156b7d20ac1a32a67ca0583
Isabel Urrutia <profesora.isabel.u.a@gmail.com>
2024-09-26 19:35:55 -0300

    hito 8 - punto 7 ok

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,15 +1,18 @@
-import {pizzaCart} from '../pizzas'
 import CartPizza from '../components/CartPizza'
 import {useContext, useState} from 'react'
 import { separador_de_miles } from "../utils/funciones_valores"
 import '../style/Cart.css'
 import MyContext from '../context/MyContext'
 import ContextToken from '../context/ContextToken'
+import { useNavigate } from 'react-router-dom';
 
 const Cart = () => {
 
     const {listaProductos, setListaProductos, totalCompra, setTotalCompra}= useContext(MyContext)
     const { token } = useContext(ContextToken);
+    const [error, setError] = useState(null);
+    const navigate = useNavigate();
+
 
     // Función para eliminar un producto del carrito
     const eliminarProducto = (name) => {
@@ -20,6 +23,33 @@ const Cart = () => {
     // Función para actualizar el total general de la compra
     const actualizarTotal = (monto) => {
         setTotalCompra((prevTotal) => prevTotal + monto);
+    };
+
+    // Función para enviar el carrito de compras al backend
+    const handleCheckout = async () => {
+    try {
+        const response = await fetch('http://localhost:5000/api/checkouts', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`, // Si es necesario enviar el token
+        },
+        body: JSON.stringify({
+          items: listaProductos, // Enviar los productos del carrito
+          total: totalCompra // Enviar el total de la compra
+        }),
+        });
+
+        const data = await response.json();
+        if (response.ok) {
+            alert("Compra realizada con éxito.");
+            navigate('/'); // Redirigir al home o alguna otra página tras la compra exitosa
+        } else {
+            setError('Error al realizar la compra: ' + data.message);
+        }
+    } catch (err) {
+            setError('Error en la solicitud: ' + err.message);
+    }
     };
 
     return (
@@ -39,9 +69,11 @@ const Cart = () => {
             </div>
             <div className='totalGeneral'>
                 <h2>Total General de la Compra: ${separador_de_miles(totalCompra)}</h2>
+                {error && <p className="error">{error}</p>} {/* Mostrar errores si los hay */}
                 <button 
                     className={token ? 'pagarCarro' : 'pagarCarroDisabled'} 
                     disabled={!token}  // Deshabilitar el botón si el token es false
+                    onClick={handleCheckout} // Llamar a la función de checkout al hacer clic
                 >
                     <strong>Pagar</strong>
                 </button>
