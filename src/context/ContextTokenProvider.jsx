2adc83c86ade5e4e2bd1416a0a835f08fbd32f8e
Isabel Urrutia <profesora.isabel.u.a@gmail.com>
2024-09-26 11:04:49 -0300

    hito 8 - punto 1 ok

--- src/context/ContextTokenProvider.jsx
+++ src/context/ContextTokenProvider.jsx
@@ -1,17 +1,61 @@
 import { useState } from "react";
-import ContextToken from "./ContextToken";  // Importa el contexto que creaste
+import ContextToken from "./ContextToken";  
 
 // Este componente provee el contexto
 const ContextTokenProvider = ({ children }) => {
-    const [token, setToken] = useState(true); // Estado del token
+  const [token, setToken] = useState(null); // Estado del token, inicia en null
+  const [email, setEmail] = useState(null) // Estado para almacenar el email del usuario
 
-  // Función para cerrar sesión, cambiando el token a false
-    const logout = () => {
-    setToken(false);
-    };
+  // Función para hacer login
+  const login = async (email, password) => {
+    try {
+        const response = await fetch('http://localhost:5000/api/auth/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ email, password })
+        });
+
+        const data = await response.json();
+        if (response.ok) {
+            setToken(data.token);  // Guardar el token en el estado
+            setEmail(data.email);  // Guardar el email en el estado
+        } else {
+            console.error('Error en el login:', data.message);
+        }
+    } catch (error) {
+        console.error('Error en la solicitud de login:', error);
+    }
+  };
+
+  // Función para hacer registro
+  const register = async (email, password) => {
+    try {
+        const response = await fetch('http://localhost:5000/api/auth/register', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ email, password })
+        });
+
+        const data = await response.json();
+        if (response.ok) {
+            setToken(data.token);  // Guardar el token en el estado
+            setEmail(data.email);  // Guardar el email en el estado
+        } else {
+            console.error('Error en el registro:', data.message);
+        }
+    } catch (error) {
+        console.error('Error en la solicitud de registro:', error);
+    }
+  };
+
+
 
     return (
-        <ContextToken.Provider value={{ token, logout }}>
+        <ContextToken.Provider value={{ token, email, login, register }}>
         {children} {/* Aquí se renderizan los componentes hijos */}
         </ContextToken.Provider>
     );
