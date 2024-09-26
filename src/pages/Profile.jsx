af18358b7a68ea908146a35570916e4f3c8ad09b
Isabel Urrutia <profesora.isabel.u.a@gmail.com>
2024-09-26 15:02:07 -0300

    hito 8 - punto 5

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,30 @@
+import {useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 import '../style/Profile.css'
+import ContextToken from '../context/ContextToken'
+
 const Profile = () => {
+    const { email, logout } = useContext(ContextToken);
+    const navigate = useNavigate(); // Para redirigir al usuario después de cerrar sesión
+
+    // Función para manejar el cierre de sesión
+    const handleLogout = () => {
+        logout(); // Llamar a la función logout del contexto
+        navigate('/login'); // Redirigir al usuario a la página de login
+    };
+
     return (
         <div className='contieneProfile'>
-            <p>maildeusuario@mail.com</p>
-            <button className='cerrarSesion'><strong>CerrarSesion</strong></button>
+            <h2>Perfil de Usuario</h2>
+            {email ? (
+                <>
+                    <p>Email: {email}</p> {/* Mostrar el email del usuario autenticado */}
+                    <button onClick={handleLogout}>Cerrar sesión</button> {/* Botón para cerrar sesión */}
+                </>
+            ) : (
+                    <p>No se ha iniciado sesión.</p> // Mensaje si no hay un usuario autenticado
+            )}
+    
         </div>
     )
 }
